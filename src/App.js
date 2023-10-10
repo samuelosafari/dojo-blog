43d509216f1065fbc55ccd98552d364ed7fca801
samuelosafari <safarisamuel531@gmail.com>
2023-10-10 10:23:59 +0200

    refactoring all my components from scratch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import "./App.css";
+import Navbar from "./Navbar";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="content">
+        <h1>Homepage</h1>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,12 +1,12 @@
 const Navbar = () => {
   return (
-    <nav className="navbar">
-      <h1>The Dojo Blog</h1>
+    <div className="navbar">
+      <h1>Dojo Blog</h1>
       <div className="links">
         <a href="/">Home</a>
         <a href="/create">New Blog</a>
       </div>
-    </nav>
+    </div>
   );
 };
 
